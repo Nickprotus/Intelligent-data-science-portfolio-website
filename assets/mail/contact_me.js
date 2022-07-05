57aec7e89cc0cd53e0cb29711fac749c0ea08cf7
Nicholas Ogada <61243999+Nickprotus@users.noreply.github.com>
2022-07-05 03:38:44 +0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://jx8w2en075.execute-api.us-east-1.amazonaws.com/default/Portfolio",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
